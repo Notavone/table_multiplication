d44b9d61d17dada40f7d76bbc04b5f54227203a0
Notavone <notavone@gmail.com>
2020-11-09 18:51:12 +0100

    modif

--- index.php
+++ index.php
@@ -5,57 +5,65 @@ if (!empty($_GET) && isset($_GET["methode"])) {
     $methode = "plus";
 }
 
-if (!empty($_GET) && isset($_GET["table"])) {
+if (!empty($_GET) && isset($_GET["table"])
+) {
     $table = $_GET["table"];
 } else {
     $table = 1;
 }
 
-$max_table = 10;
+$maxTables = 10;
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="./main.css">
     <link rel="icon" href="favicon.png">
+
     <title>Tables de multiplication</title>
 </head>
 <body>
 <header><h1>Tables de multiplication pour le primaire</h1></header>
 <nav>
     <?php
-    for ($i = 1; $i < $max_table + 1; $i += 1) {
-        echo "<a href='?table=$i&methode=$methode'>$i</a>";
+    for ($i = 1; $i < $maxTables + 1; $i += 1) {
+        echo '<a href="?table=' . $i . '&methode=' . $methode . '">' . $i . '</a>';
     }
-    echo "<a class='methode' href='?table=$table&methode=plus'>+</a>";
-    echo "<a  class='methode' href='?table=$table&methode=fois'>x</a>";
-    echo "<a  class='methode' href='?table=$table&methode=division'>÷</a>";
     ?>
 </nav>
+<aside>
+    <?php
+    echo '<a class="methode" href="?table=' . $table . '&methode=plus">+</a>';
+    echo '<a  class="methode" href="?table=' . $table . '&methode=fois">x</a>';
+    echo '<a  class="methode" href="?table=' . $table . '&methode=division">÷</a>';
+    ?>
+</aside>
 <main>
     <table>
         <thead>
         <tr>
-            <th><?php echo "La table de $table" ?></th>
+            <td>La table du <?php echo $table ?></td>
         </tr>
         </thead>
         <tbody>
         <?php
-        for ($i = 1; $i < $max_table + 1; $i += 1) {
-            $result = 0;
-            if ($methode === "plus") {
-                $result = intval($table) + intval($i);
-                echo "<tr><td>" . $table . " + " . $i . " = " . $result . "</td></tr>";
+        for ($i = 1; $i < $maxTables + 1; $i += 1) {
+            switch ($methode) {
+                case "division":
+                    $stringMethod = " ÷ ";
+                    $result = number_format(intval($table) / intval($i), 2, ',', ' ');
+                    break;
+                case "fois":
+                    $stringMethod = " x ";
+                    $result = intval($table) * intval($i);
+                    break;
+                default :
+                    $stringMethod = " + ";
+                    $result = intval($table) + intval($i);
             }
-            if ($methode === "fois") {
-                $result = intval($table) * intval($i);
-                echo "<tr><td>" . $table . " x " . $i . " = " . $result . "</td></tr>";
-            }
-            if ($methode === "division") {
-                $result = number_format(intval($table) / intval($i), 2, ',', ' ');
-                echo "<tr><td>" . $table . " ÷ " . $i . " = " . $result . "</td></tr>";
-            }
+            echo '<tr><td>' . $table . ' ' . $stringMethod . ' ' . $i . ' = ' . $result . '</td></tr>';
         }
         ?>
         </tbody>
